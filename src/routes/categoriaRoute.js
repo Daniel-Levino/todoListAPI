426f3281cea8c899fe20364daab9135735bf5e7e
Daniel Levino <daniel.barbosa@a.ficr.edu.br>
2021-04-13 11:22:49 -0300

    API funcinando

---
+++ src/routes/categoriaRoute.js
@@ -0,0 +1,13 @@
+module.exports = function (app) {
+
+   const categoriaController = require('../controllers/categoriaController')
+
+app.route('/categoriaRoute') 
+    .get(categoriaController.listAll)
+    .post(categoriaController.createOne)
+
+app.route('/categoriaRoute/:id')
+	.get(categoriaController.listOne)
+	.put(categoriaController.updateOne)	
+    .delete(categoriaController.deleteOne)     
+}
